831bca474aa8bbef3fac6432afa53f60d6f35eff
Harry-JW <146178087+Harry-JW@users.noreply.github.com>
2024-11-07 19:05:18 +0000

    Updated log in
    Added more functionality to log in and added admin main page

--- Application/app/src/main/java/com/example/finalattempt/MainActivity.java
+++ Application/app/src/main/java/com/example/finalattempt/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.finalattempt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,14 +16,15 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import java.io.Console;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button1;
-    Button ALogInButton;
-    Button ELogInButton;
-    EditText EPassword; EditText EUName;
-    String value="hello world,this is part 2";
+    Button ELogInButton;Button ALogInButton;
+    TextView Eresult;TextView AResult;
+    EditText EPassword; EditText EUName;EditText APassword; EditText AUName;
+    String CorrectUserName="Hwatton";String CorrectPassWord="12345";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,18 +35,82 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+        EUName=(EditText) findViewById(R.id.EUserName);
+        EPassword=(EditText)findViewById(R.id.EPWord);
+        Eresult=(TextView)findViewById(R.id.ELogInResult);
 
 
         ELogInButton=(Button)findViewById(R.id.EmployeeLogIn);
         ELogInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
-                startActivity(intent);
+                Eresult.setText(EUName.getText());
+                //EUName.setText("");
+                if(GetIfLogInSucessful(EUName.getText().toString(),EPassword.getText().toString(),true,Eresult)){
+                    Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                    startActivity(intent);
+                }
+                EUName.setText("");
+
             }
         });
 
-        EUName=(EditText) findViewById(R.id.EUserName);
+        AUName=(EditText)findViewById(R.id.AUserName);
+        APassword=(EditText) findViewById(R.id.EPassword);
+        ALogInButton=(Button) findViewById(R.id.AdminLogIn);
+        AResult=(TextView) findViewById(R.id.AResultText);
+
+        ALogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(GetIfLogInSucessful(AUName.getText().toString(),APassword.getText().toString(),false,AResult)){
+                    Intent intent = new Intent(MainActivity.this,adminMainPage.class);
+                    startActivity(intent);
+                }
+                else{
+                    APassword.setText("");AUName.setText("");
+                }
+            }
+        });
+
+
+    }
+    public boolean GetIfLogInSucessful(String Username,String password,Boolean IsEmployee,TextView Result){
+        if(IsEmployee){
+            if(Username.equals(CorrectUserName)){
+                Result.setText("username correct");
+                if(password.equals(CorrectPassWord))
+                {
+                    return  true;
+                }
+                else{
+                    Result.setText("Incorrect details entered");
+                    return false;
+                }
+            }
+            else{
+                Result.setText("Username not found");
+                return false;
+            }
+        }
+        else {
+            if(Username.equals(CorrectUserName)){
+                Result.setText("username correct");
+                if(password.equals(CorrectPassWord))
+                {
+                    return  true;
+                }
+                else{
+                    Result.setText("Incorrect details entered");
+                    return false;
+                }
+            }
+            else{
+                Result.setText("Username not found");
+                return false;
+            }
+        }
     }
 }
---
+++ Application/app/src/main/java/com/example/finalattempt/adminMainPage.java
@@ -0,0 +1,36 @@
+package com.example.finalattempt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class adminMainPage extends AppCompatActivity {
+    Button SignOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin_main_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        SignOut=(Button)findViewById(R.id.AdminMainPageSignOutButton);
+        SignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(adminMainPage.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
